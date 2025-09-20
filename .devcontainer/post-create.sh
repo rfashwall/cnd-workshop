32b518cc97ff82a1d0b7a29db13451ff9ab3cd6c
rfashwall <rabieh.fashwall1@galeria.com>
2025-09-20 21:39:23 +0200

    init repo

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Post-create script for Kubernetes Operators Workshop
+set -e
+
+echo "Setting up workshop environment..."
+
+# Install operator-sdk
+echo "Installing operator-sdk..."
+export ARCH=$(case $(uname -m) in x86_64) echo -n amd64 ;; aarch64) echo -n arm64 ;; *) echo -n $(uname -m) ;; esac)
+export OS=$(uname | awk '{print tolower($0)}')
+export OPERATOR_SDK_DL_URL=https://github.com/operator-framework/operator-sdk/releases/download/v1.32.0
+curl -LO ${OPERATOR_SDK_DL_URL}/operator-sdk_${OS}_${ARCH}
+chmod +x operator-sdk_${OS}_${ARCH}
+sudo mv operator-sdk_${OS}_${ARCH} /usr/local/bin/operator-sdk
+
+# Install kind (Kubernetes in Docker)
+echo "Installing kind..."
+[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-amd64
+[ $(uname -m) = aarch64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-arm64
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
+
+# Install kustomize
+echo "Installing kustomize..."
+curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh" | bash
+sudo mv kustomize /usr/local/bin/
+
+# Verify installations
+echo "Verifying tool installations..."
+go version
+kubectl version --client
+helm version
+operator-sdk version
+kind version
+kustomize version
+
+echo "Workshop environment setup complete!"
